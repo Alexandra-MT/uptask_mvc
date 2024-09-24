4e3a773a08ea009ca7e6505d915c73be14c14d97
Alexandra <alexandra.tutica@yahoo.com>
2024-09-24 14:42:53 +0200

    mostrar tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -2,19 +2,75 @@
 
 namespace Controllers;
 
+use Model\Tarea;
+use Model\Proyecto;
+
 //no necesitamos Router porque no hace falta hacer render a la vista lo hacemos mediante api
 
 class TareaController{
+    //http://localhost:3000/api/tareas?url=e6cac8450429ccc314ad083e827cae3b(un proyecto)
     public static function index(){
-        
+        session_start();
+        //debuguear($_GET);
+        $proyectoId = $_GET['url'];
+
+        if(!$proyectoId){
+            header('Location: /dashboard');
+        }
+
+        //consultamos la BBDD
+        $proyecto = Proyecto::where('url', $proyectoId);
+
+         //mira las fk
+        if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){
+            header('Location: /404');
+        }
+
+         //mira las fk
+        $tareas = Tarea::belongsTo('proyectoId', $proyecto->id);
+        //debuguear($tareas); //array de objetos
+        echo json_encode(['tareas' => $tareas]); // a formato json
+
     }
 
     public static function crear(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            
-            echo json_encode($_POST);
+        //para poder verificar si el proyecto pertenece a la persona autenticada, iniciamos session
+        session_start();
+        //debuguear($_SESSION);
+        //creamos la variable de proyectoId para compararla con el campo url de la tabla proyectos
+        //debuguear($_POST);// en el POST ya tenemos los datos de nombre y proyecto id que nos viene desde js
+        $proyectoId = $_POST['proyectoId'];
+        //verificamos si hay proyecto con ese url
+        $proyecto = Proyecto::where('url', $proyectoId);
+        //debuguear($proyecto);
+
+        //mira las fk
+        if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){//id usuario){
+            $respuesta = [
+                'tipo' => 'error',
+                'mensaje' => 'Hubo un error al agregar la tarea'
+            ];
+            echo json_encode($respuesta);
+            return; //para que no se ejecuten las siguientes lineas
         }
+
+        //todo bien , instanciar y crear la tarea
+            $tarea = new Tarea($_POST);
+             //mira las fk
+            $tarea->proyectoId = $proyecto->id;// ojo es necesario asignar el id del proyecto al proyectoId de la tarea ya que hasta aqui el valor era el url
+            $resultado = $tarea->guardar();
+            //debuguear($resultado); devuelve el id y si es true o false que se guardo
+            $respuesta = [
+                'id' => $resultado['id'],
+                'tipo' => 'exito',
+                'mensaje' => 'Tarea creada correctamente'
+            ];
+           
+            echo json_encode($respuesta);
+        
     }
+}
 
     public static function actualizar(){
         if($_SERVER['REQUEST_METHOD'] === 'POST'){
--- src/js/tareas.js
+++ src/js/tareas.js
@@ -1,8 +1,40 @@
 //iife, proteje las variables para no poder leerlo desde otro archivo y asi no haya interferencias 
 (function(){
+
+    obtenerTareas();
     //boton modal agragar tarea
     const nuevaTareaBtn = document.querySelector('#agregar-tarea');
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    async function obtenerTareas(){
+       try {
+            const urlProyecto = obtenerProyecto(); //para obtener solo la url
+            const url = `/api/tareas?url=${urlProyecto}` ;
+            const respuesta = await fetch(url); //como no es POST, no hace falta aportar datos
+            const resultado = await respuesta.json();
+
+            //console.log(resultado.tareas);
+
+            const {tareas} = resultado;
+            //console.log(tareas);
+            mostrarTareas(tareas);
+       } catch (error) {
+            console.log(error);
+       }
+    }
+
+    function mostrarTareas(tareas){
+        if(tareas.length === 0){
+            //si no hay tareas
+            const contenedorTareas = document.querySelector('#listado-tareas');
+            const textoNoTareas = document.createElement('LI');
+            textoNoTareas.textContent = 'No existen tareas';
+            textoNoTareas.classList.add('no-tareas');
+
+            contenedorTareas.appendChild(textoNoTareas);
+
+        }
+    }
 
     function mostrarFormulario(){
         const modal = document.createElement('DIV');
@@ -90,7 +122,8 @@
     async function agregarTarea(tarea){
         //formdata, construir la peticion
         const datos = new FormData();
-        datos.append('nombre' ,tarea);
+        datos.append('nombre' ,tarea); //nombre que introducimos
+        datos.append('proyectoId' , obtenerProyecto()); //la url del proyecto e6cac8450429ccc314ad083e827cae3b
 
         try {
             //async/await
@@ -99,15 +132,34 @@
                 method: 'POST',
                 body:datos
             });
-            
+            //lo que estamos leyendo
             const resultado = await respuesta.json();
-            console.log(resultado);
+           
+            mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.formulario legend'));
+            
+            if(resultado.tipo === 'exito'){ // importante que sea exitoso en el momento de guardar los datos
+                const modal = document.querySelector('.modal');
+                setTimeout(() => {
+                    modal.remove();
+                }, 3000);
+            }
 
         } catch (error) {
-            console.log(error);
+            console.log(error);// solo para servidor
         }
 
     }
+
+    function obtenerProyecto(){
+        const proyectoParams = new URLSearchParams(window.location.search);// windows.location donde te encuentras
+        //console.log(proyectoParams);
+
+        const proyecto = Object.fromEntries(proyectoParams.entries());
+        return proyecto.url;
+
+    }
+
+    
 })(); //cierre iife con ();
 
 
